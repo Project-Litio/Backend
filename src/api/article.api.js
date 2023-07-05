8a61e2099d67b451be0f3732088b2644d2e25d85
Seb0927 <sebastianidrobo@hotmail.com>
2023-07-05 14:03:36 -0500

    Updates

--- src/api/article.api.js
+++ src/api/article.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const articleApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/inventory/'
+    baseURL: 'https://backend-lithium.onrender.com/inventory/'
 });
 
 export const getAllCars = () => {
